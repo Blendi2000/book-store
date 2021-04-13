c8208635f749faec7a7d74ab51f0710fbaebb144
Blendi2000 <78365193+Blendi2000@users.noreply.github.com>
2021-04-13 11:23:36 +0200

    first-commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import 'bootstrap'
+import 'popper.js'
+import jQuery from 'jquery';
+window.$ = window.jQuery = jQuery;
+import './assets/app.scss'
+
 
 Vue.config.productionTip = false
+Vue.component('Navbar', require('./components/Navbar.vue').default);
+
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
---
+++ src/views/Products.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="products">
+<div class="container">
+   <div class="intro h-100">
+        <div class="row h-100 justify-content-center align-items-center">
+            <div class="col-md-6">
+              <h3>Books Page</h3>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ullam sunt quia similique, ea pariatur consectetur earum voluptates porro fugiat ut ex corrupti perspiciatis nihil reprehenderit unde? Quia amet quibusdam vel.</p>
+                    <img src="/img/svg/books.svg" alt="" class="img-fluid">
+
+            </div>
+            <div class="col-md-6">
+
+            </div>
+        </div>
+    </div>
+</div>
+  </div>
+  
+  
+</template>
+
+<script>
+export default {
+  name: 'Products',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
