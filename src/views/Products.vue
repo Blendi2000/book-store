d296d4f311cac87112542a04d5832405b615433d
liandaosmani <70145245+liandaosmani@users.noreply.github.com>
2021-04-19 00:57:11 +0200

    Update Products.vue

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -9,33 +9,106 @@
                     <img src="/img/svg/books.svg" alt="" class="img-fluid">
 
             </div>
-            <div class="col-md-6">
-
-            </div>
+            <!-- <div class="col-md-6">
+            </div> -->
         </div>
     </div>
     <hr>
-
-    <h3>Basic CRUD</h3>
+    
     <div class="product-test">
-      <div class="form-group">
-        <input type="text" placeholder="Product Name" v-model="product.name" class="form-control">
-      </div>
 
-      <div class="form-group">
-        <input type="text" placeholder="Price" v-model="product.price" class="form-control">
-      </div>
+      <h3 class="d-inline-block">Books List</h3>
+        <button @click="addNew" class="btn btn-primary float-right">Add Book</button><hr>
+      <div class="table-responsive">
 
-      <div class="form-group">
-        <button @click="saveData" class="btn btn-primary">Save Data</button>
+        <table class="table">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Price</th>
+            <th>Modify</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="product in products">
+            <td>
+              {{product.name}}
+              </td>
+          </tr>
+        </tbody>
+      </table>
+
       </div>
 
     </div>
 </div>
+
+  <!-- Modal -->
+      <div class="modal fade" id="product" tabindex="-1" role="dialog" aria-labelledby="editLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="editLabel">Add Book</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+
+                <div class="row">
+                  <!-- main product -->
+                  <div class="col-md-8">
+                    <div class="form-group">
+                      <input type="text" placeholder="Book Name" v-model="product.name" class="form-control">
+                    </div>
+
+                    <div class="form-group">
+                      <textarea name="description" placeholder="Book Description" v-model="product.description" class="form-control"></textarea>
+                    </div>
+                  </div>
+                  <!-- product sidebar -->
+                  <div class="col-md-4">
+                    <h4 class="display-6">Book Details</h4>
+                    <hr>
+
+                    <div class="form-group">
+                      <input type="text" placeholder="Book price" v-model="product.price" class="form-control">
+                    </div>
+
+                    <div class="form-group">
+                      <input type="text" placeholder="Book tags" v-model="product.tag" class="form-control">
+                      
+                    </div>
+
+
+                    <div class="form-group">
+                      <label for="product_image">Book Images</label>
+                      <input type="file" @change="uploadImage" class="form-control">
+                    </div>
+                    <!-- <div class="form-group d-flex">
+                      <div class="p-1" v-for="(image, index) in product.images">
+                          <div class="img-wrapp">
+                              <img :src="image" alt="" width="80px">
+                              <span class="delete-img" @click="deleteImage(image,index)">X</span>
+                          </div>
+                      </div>
+                    </div> -->
+
+                  </div>
+                </div>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+              <button @click="addProduct()" type="button" class="btn btn-primary">Save changes</button>
+            </div>
+          </div>
+        </div>
+      </div>
   </div>
 </template>
 
 <script>
+/*eslint-disable */
 
 import {fb, db} from '../firebase';
 
@@ -46,27 +119,44 @@ export default {
   },
   data(){
     return{
+      products: [],
       product:{
         name: null,
-        price: null
-      }
+        description: null,
+        price: null,
+        tag: null,
+        image: null,
+      },
+      activeItem:null
     }
   }, 
-  methods:{
-    saveData(){
-
-      db.collection("products").add(this.product)
-      .then((docRef) => {
-        console.log("Document written with ID: ", docRef.id);
-        this.reset();
-      })
-      .catch(function(error){
-        console.error("Error adding document: ", error);
-      });
-    },
-    reset(){
-      Object.assign(this.$data, this.$options.data.apply(this))
+  firestore(){
+    return{
+      products: db.collection('products'),
     }
+  },
+  methods:{
+    uploadImage(){},
+
+    addNew(){
+        //this.modal = 'new';
+        //this.reset();
+        $('#product').modal('show');
+    },
+    updateProduct(){
+    },
+    editProduct(product){
+    },
+    deleteProduct(doc){
+    },
+    readData(){
+    },
+    addProduct(){
+      this.$firestore.products.add(this.product);
+      $('#product').modal('hide');
+    }
+  },
+  created(){
   }
 };  
 </script>
