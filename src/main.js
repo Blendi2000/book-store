cf643341c70ff2043d1ec5b91dee37616c3d5c87
liandaosmani <70145245+liandaosmani@users.noreply.github.com>
2021-04-19 00:58:14 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -6,13 +6,27 @@ import 'popper.js'
 import jQuery from 'jquery';
 window.$ = window.jQuery = jQuery;
 import './assets/app.scss'
+import {fb} from './firebase';
+import VueFirestore from 'vue-firestore'
+
+Vue.use(VueFirestore)
 
 
 Vue.config.productionTip = false
 Vue.component('Navbar', require('./components/Navbar.vue').default);
 
+let app='';
+fb.auth().onAuthStateChanged(function(user){
 
-new Vue({
-  router,
-  render: h => h(App)
-}).$mount('#app')
+    if(!app){
+      new Vue({
+        router,
+        render: h => h(App)
+      }).$mount('#app')
+      
+    }
+
+
+});
+
+
