efbe719bba0713f21e06c7904284e50518722c7a
liandaosmani <70145245+liandaosmani@users.noreply.github.com>
2021-04-18 18:31:09 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,7 @@
 import firebase from 'firebase'
+require ("firebase/firestore");
 
-
-var firebaseConfig = {
+var config = {
     apiKey: "AIzaSyCIBk9KP1Qug6jHnjEJ8xavnHNVE50dQH0",
     authDomain: "book-store-7d7b5.firebaseapp.com",
     databaseUrl:'https://book-store-7d7b5-default-rtdb.europe-west1.firebasedatabase.app/',
@@ -10,5 +10,9 @@ var firebaseConfig = {
     messagingSenderId: "376036731901",
     appId: "1:376036731901:web:033060b06eae6552390794"
   };
+  
+  const fb = firebase.initializeApp(config);
+  const db = firebase.firestore();
+  export {fb,db}
   // Initialize Firebase
-  export const fb = firebase.initializeApp(firebaseConfig);
+  //export const fb = firebase.initializeApp(firebaseConfig);
