d1d5ccde8e967bbd932c57edf0d8ad052438afa7
liandaosmani <70145245+liandaosmani@users.noreply.github.com>
2021-04-19 18:49:52 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -8,9 +8,27 @@ window.$ = window.jQuery = jQuery;
 import './assets/app.scss'
 import {fb} from './firebase';
 import VueFirestore from 'vue-firestore'
+require('firebase/firestore')
+
+Vue.use(VueFirestore, {
+  key: 'id',
+  enumerable: true
+})
 
 Vue.use(VueFirestore)
 
+import Swal from 'sweetalert2';
+
+window.Swal = Swal;
+
+const Toast = Swal.mixin({
+  toast: true,
+  position: 'top-end',
+  showConfirmButton: false,
+  timer: 3000
+});
+
+window.Toast = Toast;
 
 Vue.config.productionTip = false
 Vue.component('Navbar', require('./components/Navbar.vue').default);
