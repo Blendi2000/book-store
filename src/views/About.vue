c106d054b4a1bdc0475498d366e9cf241ea47d64
liandaosmani <70145245+liandaosmani@users.noreply.github.com>
2021-04-29 17:52:26 +0200

    Update About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -24,10 +24,27 @@
       </div>
       <Login></Login>
     <mini-cart></mini-cart>
-
+    <hr>
+<footer class="page-footer">
+  <div class="container">
+    <br>
+    <div class="row">
+      <div class="col-lg-8 col-md-8 col-sm-12">
+      <h6 class="text-uppercase font-weight-bold">Additional Information</h6><br>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque interdum quam odio, quis placerat ante luctus eu. Sed aliquet dolor id sapien rutrum, id vulputate quam iaculis.</p>
+    </div>
+    <div class="col-lg-4 col-md-4 col-sm-12">
+      <h6 class="text-uppercase font-weight-bold">Contact</h6>
+      <p>Lagjia Kalabria p.n
+      <br/>info@mywebsite.com
+      <br/>+ 383 38 541 400
+      <br/>+ 383 44 541 400</p>
+    </div>
   </div>
-
-  
+  </div>
+  <div class="footer-copyright text-center">© 2021 Copyright: Bookstore.com</div>
+</footer>
+</div>  
 </template>
 
 <script>
