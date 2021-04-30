9e370762c44569625746608718b78642ee3dae4e
erikkastrati <80789499+erikkastrati@users.noreply.github.com>
2021-05-01 00:41:30 +0200

    Update Admin.vue

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -25,7 +25,7 @@
                         <span class="user-role"> {{email}} </span>
                         <span class="user-status">
                             <i class="fa fa-circle"></i>
-                            <span>ONLINE</span>
+                            <span>Online</span>
                         </span>
                     </div>
                 </div>
