331f2314af633782fb5e0f08d45af920bddbdb92
lumaliuu <73234838+lumaliuu@users.noreply.github.com>
2021-05-03 15:52:49 +0200

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,7 +10,7 @@
             <img src="/img/svg/product.svg" alt="" width="400">
           </div>
           <div class="col-md-6">
-            <h3>Learn More About us</h3>
+            <h3>Learn More About Us</h3>
             <p>
               Lorem ipsum dolor sit amet consectetur adipisicing elit.  Nihil voluptas vitae veritatis magnam reprehenderit, reiciendis ex? Libero quasi natus veniam voluptatum, voluptas exercitationem ratione consectetur et!
             </p>
